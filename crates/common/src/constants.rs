d4bd10be1406b590cef5f7a79b406b5d3df3965e
Christian Pavilonis <christianpavilonis@outlook.com>
2025-11-10 11:04:57 -0800

    Change x-synthetic-trusted-server header to x-psid-ts (#66)

--- crates/common/src/constants.rs
+++ crates/common/src/constants.rs
@@ -3,8 +3,7 @@ use http::header::HeaderName;
 pub const HEADER_SYNTHETIC_FRESH: HeaderName = HeaderName::from_static("x-synthetic-fresh");
 pub const HEADER_SYNTHETIC_PUB_USER_ID: HeaderName = HeaderName::from_static("x-pub-user-id");
 pub const HEADER_X_PUB_USER_ID: HeaderName = HeaderName::from_static("x-pub-user-id");
-pub const HEADER_SYNTHETIC_TRUSTED_SERVER: HeaderName =
-    HeaderName::from_static("x-synthetic-trusted-server");
+pub const HEADER_SYNTHETIC_TRUSTED_SERVER: HeaderName = HeaderName::from_static("x-psid-ts");
 pub const HEADER_X_CONSENT_ADVERTISING: HeaderName =
     HeaderName::from_static("x-consent-advertising");
 pub const HEADER_X_FORWARDED_FOR: HeaderName = HeaderName::from_static("x-forwarded-for");
